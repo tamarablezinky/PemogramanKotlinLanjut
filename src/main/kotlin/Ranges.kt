7d147ca4dc72fb88b10dad10c658285f19d84b3c
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-09 17:41:09 +0700

    Add Ranges.kt

---
+++ src/main/kotlin/Ranges.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //Ranges
+    val i: Int = 2
+    for (j in 1..4)
+        print(j) //print "1234"
+    println()
+
+    if (i in 1..10)
+        println ("we found your number -- "+i)
+}
