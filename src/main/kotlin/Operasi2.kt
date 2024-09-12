a4e561f21a4d0f604939f4cbe46735f0a3b2b2ff
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-12 10:32:33 +0700

    Add Operator2.kt

---
+++ src/main/kotlin/Operasi2.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    //Null safety
+    //The !! operator
+    var maybeNumber: Int? = 15
+    println(maybeNumber!! * 2)
+
+    maybeNumber = null
+    println(maybeNumber!! * 2)
+} 
